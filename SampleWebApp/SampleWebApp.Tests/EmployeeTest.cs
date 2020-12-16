cd6df3a243edf3354f1b5367671a6afd723bd3d7
sangeethathamil80 <sangeetha.thamil@gmail.com>
2020-12-16 10:42:29 +0530

    code checkin

---
+++ SampleWebApp/SampleWebApp.Tests/EmployeeTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleWebApp.Controllers;
+using SampleWebApp.Models;
+
+namespace SampleWebApp.Tests
+{
+    [TestClass]
+    public class EmployeeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var employeeList = GetAllEmployees();
+
+            EmployeeController controller = new EmployeeController();
+
+            var result = controller.GetAllEmployees();
+            Assert.AreEqual(employeeList.Count, result.Count);
+        }
+
+        private List<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+            new Employee()
+                {
+                    EmployeeId = 1, EmployeeName = "Sangeetha", Address = "Chennai", Department = "IT"
+                },
+                new Employee()
+                {
+                    EmployeeId = 2, EmployeeName = "BulBuli Swain", Address = "Chennai", Department = "HR"
+                },
+                new Employee()
+                {
+                    EmployeeId = 3, EmployeeName = "Manickam", Address = "Thanbaram", Department = "Tech Lead"
+                },
+                new Employee()
+                {
+                    EmployeeId = 4, EmployeeName = "Kalai", Address = "Guduvanchery", Department = "Senior Developer"
+                },
+                new Employee()
+                {
+                    EmployeeId = 5, EmployeeName = "Sowmiya", Address = "Trichy", Department = "HR"
+                },
+            };
+
+            return employees;
+        }
+    }
+}
